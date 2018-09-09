a70f1aef91545a824aeb6a5d9e845a94987fef1a
JJ Crosskey <jjcrosskey@quantmt.com>
2018-09-09 11:13:27 -0400

    Add url, view for new_list, fix redirect

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
-    re_path('^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    ## No trailing slash -- action URL that modifies the database (convention)
+    path('lists/new', views.new_list, name='new_list'),
 ]
